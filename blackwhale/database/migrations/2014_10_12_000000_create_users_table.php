fe5e5e41a6177846259215c21fcbd82937fd39ca
DESKTOP-9PUL31I\asus <wengkee101@gmail.com>
2020-07-27 14:45:27 +0800

    khew file

---
+++ blackwhale/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 191)->nullable()->unique();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->integer('postcode');
+            $table->string('state');
+            $table->string('personincharge');
+            $table->string('contact');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+    
+}
